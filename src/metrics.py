Fox trajectory prediction a little, a bug in the direction and dist calculation. :bug: :ambulance:

src/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-
+import cv2
 import pandas as pd
 from mean_average_precision import MetricBuilder
 from src.review_object_detection_metrics.src.evaluators.coco_evaluator import get_coco_summary, get_coco_metrics
@@ -10,7 +10,7 @@ from src.review_object_detection_metrics.src.utils.enumerators import BBFormat, 
 
 
 class Metrics:
-    def __init__(self, labels, image_names, num_of_frames, iou_threshold=0.5):
+    def __init__(self, labels, image_names, num_of_frames, debug=False):
         self.labels = labels
         self.image_names = image_names
         self.num_of_frames = num_of_frames
@@ -28,17 +28,17 @@ class Metrics:
         self.map_frequency = 200
         self.default_metrics = {}
         self.coco_summary = {}
-        self.iou_threshold = iou_threshold
-        self.trajectory_metric_summary= []
+        self.trajectory_metric_summary = []
+        self.debug = debug
 
-        print('labels num:', len(self.labels), 'iou_threshold:',  self.iou_threshold)
+        print('labels num:', len(self.labels))
 
     def reset_labels(self, labels):
         self.labels = labels
         self.frame_counter = 0
 
     def update(self, yolo_predictions, predictions_split, start_time, yolo_end_time, tracking_end_time, camera_motion_estimation_end_time,
-               trajectory_prediction_end_time):
+               trajectory_prediction_end_time, frame):
         """Both predictions and validation_data are in format: [x1, y1, x2, y2] (top left, right bottom)
         Where predictions are a list of frames, each frame has a list of tracked objects, each object has 3 bbs: each one for the prediction step [1, 3, 10]
         """
@@ -46,6 +46,14 @@ class Metrics:
         self.yolo_predictions.append(self.preds_to_map_format(yolo_predictions, self.yolo_preds_to_map_format))
         for i, predictions in enumerate(predictions_split):
             self.predictions[i].append(self.preds_to_map_format(predictions, self.yolo_preds_to_map_format))
+
+        self.show_future_labels(frame, 1)
+        self.show_future_labels(frame, 3)
+        self.show_future_labels(frame, 10)
+
+        # Debug
+        if self.debug:
+            self.debug_trajectory_pred(yolo_predictions, frame)
 
         # print(len(self.predictions[0]), self.predictions[0])
 
@@ -84,20 +92,45 @@ class Metrics:
         if self.frame_counter == self.num_of_frames - 1:
             self.calculate_final_map(self.yolo_predictions)
 
+            print('------------------- step 1 ------------------')
+            self.calculate_final_map(self.predictions[0], step=1)
+            print('------------------- step 3 ------------------')
+            self.calculate_final_map(self.predictions[1], step=3)
+            print('------------------- step 10 ------------------')
+            self.calculate_final_map(self.predictions[2], step=10)
 
-            # print('------------------- step 1 ------------------')
-            # self.calculate_final_map(self.predictions[0], step=1)
-            # print('------------------- step 3 ------------------')
-            # self.calculate_final_map(self.predictions[1], step=3)
-            # print('------------------- step 10 ------------------')
-            # self.calculate_final_map(self.predictions[2], step=10)
-            #
-            # self.print_trajectory_metric_summary()
+            self.print_trajectory_metric_summary()
             # self.calculate_final_map_v2() # more official metric repo
 
         self.metric_counter += 1
         self.frame_counter += 1
         self.total_frame_counter += 1
+        return frame
+
+    def show_future_labels(self, frame, step):
+        labels_formatted = np.array([self.label_to_bb(label) for label in self.labels if int(label['frame']) == self.total_frame_counter + step])
+        for bb in labels_formatted:
+            cv2.rectangle(frame, (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3])), (0, 255, 0), 1)  # Green
+
+    def debug_trajectory_pred(self, yolo_predictions, frame):
+        print(f'--------- frame {self.total_frame_counter}---------')
+
+        labels_formatted = np.array([self.label_to_bb(label) for label in self.labels if int(label['frame']) == self.total_frame_counter])
+        print('yolo pred:', yolo_predictions, 'labels:', labels_formatted)
+
+        if self.predictions[0][-1].size == 0:
+            return
+
+        labels_formatted = np.array([self.label_to_bb(label) for label in self.labels if int(label['frame']) == self.total_frame_counter + 1])
+        print(self.total_frame_counter + 1, 'pred:', self.predictions[0][-1], 'label:', labels_formatted)
+
+        labels_formatted = np.array([self.label_to_bb(label) for label in self.labels if int(label['frame']) == self.total_frame_counter + 3])
+        print(self.total_frame_counter + 3, 'pred:', self.predictions[1][-1], 'label:', labels_formatted)
+
+        labels_formatted = np.array([self.label_to_bb(label) for label in self.labels if int(label['frame']) == self.total_frame_counter + 10])
+        print(self.total_frame_counter + 10, 'pred:', self.predictions[2][-1], 'label:', labels_formatted)
+        for bb in labels_formatted:
+            cv2.rectangle(frame, (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3])), (0, 255, 0), 1)  # Green
 
     def summarize(self):
         errors = [np.abs(real - prediction) for real, prediction in zip(self.real_direction_angles, self.predicted_direction_angles)]
@@ -124,7 +157,7 @@ class Metrics:
 
             # labels_formatted = np.array([self.label_to_map_format(label) for label in self.labels if int(label['frame']) == i])
             labels_formatted = np.array([self.label_to_map_format(label) for label in self.labels if int(label['frame']) == i + step])
-            # print(i, preds, labels_formatted)
+            print(i, preds, labels_formatted)
 
             gt_frame_nums_list += [label['frame'] for label in self.labels if int(label['frame']) == i]
             num_of_pred_bbs += len(preds)
@@ -142,7 +175,6 @@ class Metrics:
         map50 = map_50_res['mAP']
         map_50_095 = map_50_095_res['mAP']
 
-
         tp = map_50_res['tp']
         fp = map_50_res['fp']
         total_positives = map_50_res['tpfn']
@@ -152,7 +184,7 @@ class Metrics:
         f1_score = 2 * (precision * recall) / (precision + recall)
 
         if trajectory_prediction:
-            self.default_metrics = {'step':step, 'mAP50': map50, 'mAP50_95': map_50_095, 'Precision': precision, 'Recall': recall, 'F1_score': f1_score}
+            self.default_metrics = {'step': step, 'mAP50': map50, 'mAP50_95': map_50_095, 'Precision': precision, 'Recall': recall, 'F1_score': f1_score}
             self.trajectory_metric_summary.append(self.default_metrics)
         else:
             self.default_metrics = {'mAP50': map50, 'mAP50_95': map_50_095, 'Precision': precision, 'Recall': recall, 'F1_score': f1_score}
@@ -160,7 +192,6 @@ class Metrics:
         print('---------- COCO default metrics ---------------')
         print(self.default_metrics)
         print('total positives:', total_positives, 'tp:', tp, 'fp:', fp)
-
 
     def calculate_final_map_v2(self):
         """Calculate COCO metrics
@@ -252,12 +283,16 @@ class Metrics:
         """gt (labels) -  [xmin, ymin, xmax, ymax, class_id, difficult, crowd]"""
         return [label['left'], label['top'], label['right'], label['bottom'], 0, 0, 0]
 
+    def label_to_bb(self, label):
+        """gt (labels) -  [xmin, ymin, xmax, ymax]"""
+        return [label['left'], label['top'], label['right'], label['bottom']]
+
     def yolo_preds_to_map_format(self, yolo_pred):
         """
         :param yolo_pred: [xmin, ymin, xmax, ymax, confidence]
         :return:  [xmin, ymin, xmax, ymax, class_id, confidence]
         """
-        return [yolo_pred[0], yolo_pred[1], yolo_pred[2], yolo_pred[3], 0, yolo_pred[4]]
+        return [yolo_pred[0], yolo_pred[1], yolo_pred[2], yolo_pred[3], 0, yolo_pred[4]] if len(yolo_pred)>0 else []
 
     def plot_precision_recall_curve(self, precisions, recalls):
         decreasing_max_precisions = np.maximum.accumulate(precisions[::-1])[::-1]

src/predict.py
@@ -13,7 +13,7 @@ from PIL import Image
 from src.sort_tracking import *
 import numpy as np
 from src.load_utils import get_kitti_tracking_img_filepaths, get_kitti_tracking_labels, get_frame_labels, draw_arrow_from_angle, vector_to_angle, \
-    draw_example_arrows, get_center_x, get_center_y, transform_bb, m_to_xy, draw_arrow_from_m, get_kitti_tracking_labels_multiple_recordings, \
+    draw_example_arrows, get_center_x, get_center_y, transform_bb, draw_arrow_from_m, get_kitti_tracking_labels_multiple_recordings, \
     merge_kitti_tracking_labels_multiple_recordings, get_kitti_tracking_labels_with_img_names
 from src.metrics import Metrics
 from src.camera_motion_estimator import CameraMotionEstimator
@@ -209,7 +209,7 @@ def predict_video_yolov4(input_video_path, output_video_path, weights_path, conf
 
 # --------------------------------------------------------------------
 def predict_video_from_frames_yolo(src_frames_dir, src_labels_dir, recording_nums, output_video_path, weights_path, config_path, model_type, conf_threshold=0.4,
-                                   nms_threshold=0.6, max_age=5, min_hits=3, sort_iou_threshold=0.3, show_frames=True):
+                                   nms_threshold=0.6, max_age=5, min_hits=3, sort_iou_threshold=0.3, show_frames=True, debug=False):
     """
 
     :param src_frames_dir: path to images
@@ -258,7 +258,7 @@ def predict_video_from_frames_yolo(src_frames_dir, src_labels_dir, recording_num
     print(merged_image_names)
 
     print('Frame counts:', num_of_frames, num_of_frames_list)
-    metrics = Metrics(merged_labels, merged_image_names, num_of_frames)
+    metrics = Metrics(merged_labels, merged_image_names, num_of_frames, debug=debug)
 
     for recording_num in recording_nums:
         img_filepaths = get_kitti_tracking_img_filepaths(src_frames_dir, recording_num)
@@ -270,7 +270,7 @@ def predict_video_from_frames_yolo(src_frames_dir, src_labels_dir, recording_num
         mot_tracker = Sort(max_age=max_age,
                            min_hits=min_hits,
                            iou_threshold=sort_iou_threshold)
-        trajectory_model = TrajectoryModel(width, height)
+        trajectory_model = TrajectoryModel(width, height, debug=debug)
         camera_motion_estimator = CameraMotionEstimator(img_filepaths, width, height)
 
         for f in img_filepaths:
@@ -314,19 +314,20 @@ def predict_video_from_frames_yolo(src_frames_dir, src_labels_dir, recording_num
             draw_example_arrows(frame)
 
             # Get vectors estimating shift for each tracked object on the frame due to camera motion
-            # frame, correction_vectors = camera_motion_estimator.update(frame, tracked_bbs)
+            frame, correction_vectors = camera_motion_estimator.update(frame, tracked_bbs)
+            correction_vectors = []
             camera_motion_estimation_end_time = time.time()
 
             # Predict Cyclist Trajectory
-            # predictions, predictions_split, frame = trajectory_model.predict_trajectory(mot_tracker, correction_vectors, frame)
-            predictions_split = []
+            predictions, predictions_split, frame = trajectory_model.predict_trajectory(mot_tracker, correction_vectors, frame)
+            # predictions_split = []
 
             trajectory_prediction_end_time = time.time()
 
             # normal
             # Update metrics (mAP, timers etc)
-            metrics.update(detections, predictions_split, start_time, yolo_end_time, tracking_end_time, camera_motion_estimation_end_time,
-                           trajectory_prediction_end_time)
+            frame = metrics.update(detections, predictions_split, start_time, yolo_end_time, tracking_end_time, camera_motion_estimation_end_time,
+                           trajectory_prediction_end_time, frame)
 
             # sort testing
             # metrics.update(tracked_bbs, predictions_split, start_time, yolo_end_time, tracking_end_time, camera_motion_estimation_end_time,
@@ -337,50 +338,15 @@ def predict_video_from_frames_yolo(src_frames_dir, src_labels_dir, recording_num
             if show_frames:
                 cv2.imshow('frame', frame)
                 c = cv2.waitKey(1)  # video
+                # c = cv2.waitKey(100)  # frame  by frame
                 # c = cv2.waitKey(0)  # frame  by frame
+
                 if c & 0xFF == ord('q'):
                     break
 
     # out.release()
     cv2.destroyAllWindows()
     return metrics.default_metrics, metrics.coco_summary
-
-
-def predict_trajectory(mot_tracker, correction_vectors, frame):
-    predictions = []
-    num_of_past_bbs = 5
-
-    for tracker in mot_tracker.trackers:
-        history_len = len(tracker.observed_history)
-        bb_id = min(history_len, num_of_past_bbs)  # at last of 5 tracker positions
-        if not tracker.observed_history[-bb_id:]:
-            continue
-
-        center_xs, center_ys, widths, heights = zip(*[transform_bb(bb) for bb in tracker.observed_history[-bb_id:]])
-
-        direction = 1 if center_xs[-1] - center_xs[0] > 0 else -1
-        dist = np.sqrt((center_xs[-1] - center_xs[0]) ** 2 + (center_ys[-1] - center_ys[0]) ** 2) / num_of_past_bbs
-        m, b = np.polyfit(center_xs, center_ys, 1)
-        pred_x, pred_y = m_to_xy(center_xs[-1], center_ys[-1], m, dist, direction)
-
-        draw_arrow_from_m(frame, center_xs[-1], center_ys[-1], m, dist, direction, color=(0, 255, 0))  # Green
-
-        correction_vector_temp = [vector['vector'] for vector in correction_vectors if tracker.id == vector['id']]
-        if correction_vector_temp and correction_vector_temp[0]:
-            correction_vector = correction_vector_temp[0]
-            corrected_pred_x = pred_x + correction_vector[0]
-            corrected_pred_y = pred_y + correction_vector[1]
-            # print('Raw pred:', pred_x, pred_y, '\tCorrection vector:', correction_vector, '\tCorrected pred:', corrected_pred_x, corrected_pred_y)
-            cv2.arrowedLine(frame, (int(center_xs[-1]), int(center_ys[-1])), (int(corrected_pred_x), int(corrected_pred_y)), (0, 255, 255), 2,
-                            tipLength=0.4)  # Yellow
-
-        prediction = {
-            'center_x': 0,
-            'center_y': 0,
-        }
-        predictions.append(prediction)
-
-    return predictions, frame
 
 
 # TODO: Add deepsort to project

src/trajectory_model.py
@@ -1,21 +1,25 @@
 import numpy as np
-from src.load_utils import transform_bb, m_to_xy, draw_arrow_from_m, draw_bb, draw_arrow_from_xy
+from src.load_utils import transform_bb, draw_arrow_from_m, draw_bb, draw_arrow_from_xy
 import cv2
 
 
 class TrajectoryModel:
-    def __init__(self, image_width, image_height):
+    """
+    A simple cyclist trajectory prediction model
+    """
+
+    def __init__(self, image_width, image_height, debug):
         self.tracked_bbs = []
         self.raw_predictions = []
         self.predict_steps = [1, 3, 10]  # predict values for the next 1, 3 and 10 frames
         self.image_width = image_width
         self.image_height = image_height
-
+        self.max_num_of_past_bbs = 3
+        self.debug = debug
 
     def predict_trajectory(self, mot_tracker, correction_vectors, frame):
         all_bb_predictions = []
         all_bb_predictions_split = [[], [], []]
-        num_of_past_bbs = 5
 
         self.image_width = frame.shape[1]
         self.image_height = frame.shape[0]
@@ -24,31 +28,54 @@ class TrajectoryModel:
 
         for tracker in filered_trackers:
             history_len = len(tracker.observed_history)
-            bb_id = min(history_len, num_of_past_bbs)  # at last of 5 tracker positions
-            if len(tracker.observed_history[-bb_id:]) < 2:
+            num_of_past_bbs = min(history_len, self.max_num_of_past_bbs)  # at last of 5 tracker positions
+            if num_of_past_bbs < 2:
                 continue
 
-            center_xs, center_ys, widths, heights, scores = zip(*[transform_bb(bb) for bb in tracker.observed_history[-bb_id:]])
+                # if len(tracker.observed_history[-num_of_past_bbs:]) < 1:
+            #     continue
 
-            # Predict for the next [1, 3, 10] frames (keep width and height of the bb the same as in the last frame)
+            center_xs, center_ys, widths, heights, scores = zip(*[transform_bb(bb) for bb in tracker.observed_history[-num_of_past_bbs:]])
+
+            # Interpolate future bb width and height from previous bbs - unstable and prone to noise
             # bb_width_predictions = self.predict_linear_values(widths)
             # bb_height_predictions = self.predict_linear_values(heights)
 
+            # Predict for the next [1, 3, 10] frames (keep width and height of the bb the same as in the last frame)
             bb_width_predictions = [widths[-1]] * 3
             bb_height_predictions = [heights[-1]] * 3
+            bb_scores = [scores[-1]] * 3
+
             bb_center_predictions = self.predict_bb_centers(frame, tracker, center_xs, center_ys, correction_vectors, num_of_past_bbs)
 
-            bb_preds = self.center_w_h_to_bbs(bb_center_predictions, bb_width_predictions, bb_height_predictions, scores)
-            bb_preds = [self.truncate_bb(bb) for bb in bb_preds]
-            # split predictions into separate lists
+            bb_preds = self.center_w_h_to_bbs(bb_center_predictions, bb_width_predictions, bb_height_predictions, bb_scores)
+            # bb_preds = [self.truncate_bb(bb) for bb in bb_preds]
+
+            if self.debug:
+                dist_x = np.abs(center_xs[-1] - center_xs[0]) / num_of_past_bbs
+                print('len center_xs', len(center_xs), ', num_of_past_bbs:', num_of_past_bbs, 'total dist x:', np.abs(center_xs[-1] - center_xs[0]),
+                      'scaled_dist_x', dist_x)
+                print('widths, heights, scores:', bb_width_predictions, bb_height_predictions, bb_scores)
+                print('bb_center_predictions:', bb_center_predictions)
+                print('bb_preds:', bb_preds)
+
+            # Draw circles for past bb centers
+            for x, y in zip(center_xs, center_ys):
+                cv2.circle(frame, (int(x), int(y)), 5, (255, 255, 0), 1)
+
+            # if bb_preds[2]:
+            #     draw_bb(frame, bb_preds[2])
 
             # Draw prediction bbs for next: 1, 3, 10 frames
-            for bb in bb_preds:
-                draw_bb(frame, bb)
+            # for bb in bb_preds:
+            #     if not bb:
+            #         continue
+            #     draw_bb(frame, bb)
 
             # Draw prediction arrows for next: 1, 3, 10 frames
-            for center in bb_center_predictions:
-                draw_arrow_from_xy(frame, center_xs[-1], center_ys[-1], center[0], center[1])
+            for x, y in bb_center_predictions:
+                cv2.circle(frame, (int(x), int(y)), 5, (0, 0, 255), 1)
+                # draw_arrow_from_xy(frame, center_xs[-1], center_ys[-1], center[0], center[1])
 
             all_bb_predictions.append(bb_preds)
             for i, bb in enumerate(bb_preds):
@@ -56,19 +83,23 @@ class TrajectoryModel:
 
         return all_bb_predictions, all_bb_predictions_split, frame
 
-    def predict_bb_centers(self, frame, tracker, center_xs, center_ys,  correction_vectors, num_of_past_bbs):
+    def predict_bb_centers(self, frame, tracker, center_xs, center_ys, correction_vectors, num_of_past_bbs):
         direction = 1 if center_xs[-1] - center_xs[0] > 0 else -1
-        dist = np.sqrt((center_xs[-1] - center_xs[0]) ** 2 + (center_ys[-1] - center_ys[0]) ** 2) / num_of_past_bbs
+
+        # calculate mean dist x per step. We divide it by nu of past bbs - 1, because for example, if there were 5 center points, then to the 5th point we took 4 steps only.
+        mean_dist_x = np.abs(center_xs[-1] - center_xs[0]) / (num_of_past_bbs - 1)
+
         m, b = np.polyfit(center_xs, center_ys, 1)
-        pred_centers = [self.predict_next_bb_center(frame, tracker, center_xs, center_ys, correction_vectors, m, dist, direction, step) for step in self.predict_steps]
+        pred_centers = [self.predict_next_bb_center(frame, tracker, center_xs, center_ys, correction_vectors, m, b, mean_dist_x, direction, step) for step in
+                        self.predict_steps]
 
         # print('Raw pred:', pred_x, pred_y, '\tCorrection vector:', correction_vector, '\tCorrected pred:', corrected_pred_x, corrected_pred_y)
         # cv2.arrowedLine(frame, (int(center_xs[-1]), int(center_ys[-1])), (int(corrected_pred_x), int(corrected_pred_y)), (0, 255, 255), 2,
         #                 tipLength=0.4)  # Yellow
         return pred_centers
 
-    def predict_next_bb_center(self, frame, tracker, center_xs, center_ys, correction_vectors, m, dist, direction, step):
-        pred_x, pred_y = m_to_xy(center_xs[-1], center_ys[-1], m, dist * step, direction)
+    def predict_next_bb_center(self, frame, tracker, center_xs, center_ys, correction_vectors, m, b, dist, direction, step):
+        pred_x, pred_y = self.m_to_xy(center_xs[-1], center_ys[-1], m, b, dist * step, direction)
 
         # Correct vectors with camera movement estimation
         correction_vector_temp = [vector['vector'] for vector in correction_vectors if tracker.id == vector['id']]
@@ -77,6 +108,22 @@ class TrajectoryModel:
             pred_x = pred_x + correction_vector[0] * step
             pred_y = pred_y + correction_vector[1] * step
         return pred_x, pred_y
+
+    def m_to_xy(self, x1, y1, m, b, dist, direction):
+        x2 = x1 + dist * direction
+        y2 = m * x2 + b
+
+        return x2, y2
+
+    # old
+    # def m_to_xy(self, x1, y1, m, dist, direction):
+    #     dist_scaled = dist / 1
+    #     dx = dist_scaled / np.sqrt(1 + (m * m))
+    #     dy = m * dx
+    #
+    #     x2 = x1 + dx * direction
+    #     y2 = y1 + dy
+    #     return x2, y2
 
     def predict_linear_values(self, values):
         """
@@ -100,13 +147,23 @@ class TrajectoryModel:
 
     def center_w_h_to_bbs(self, centers, widths, heights, scores):
         return [[center[0] - width / 2, center[1] - height / 2, center[0] + width / 2, center[1] + height / 2, score] for center, width, height, score in
-                zip(centers, widths, heights, scores) if self.check_boundries(center)]
+                zip(centers, widths, heights, scores)]
 
     def truncate_bb(self, bb):
-        """left top is [0,0], right bottom is [width, height]"""
-        return [max(bb[0], 0), max(bb[1], 0), min(bb[2], self.image_width), min(bb[3], self.image_height), bb[4]]
+        """
+        Remove bb if it's out of frame.
+        If its partially out of frame then truncate it.
+        left top is [0,0], right bottom is [width, height]
 
-    def check_boundries(self, center):
-        """Check if prediction bb center is inside of the frame"""
-        return center[0]>0 and center[0]<self.image_width and center[1]>0 and center[1]<self.image_height
+        :param bb: [left, top, right, bottom, score]
+        :return:
+        """
+        left, top, right, bottom, score = bb
 
+        if right < 0 or left > self.image_width:
+            return []
+
+        if bottom < 0 or top > self.image_height:
+            return []
+
+        return [max(left, 0), max(top, 0), min(right, self.image_width), min(bottom, self.image_height), score]